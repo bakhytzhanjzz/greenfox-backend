3020baf036099dfcb3d9b8f8c7854f35ed23c61f
bakhytzhanjzz <bakhytzhanabdilmazhit@gmail.com>
2025-07-13 23:57:45 +0500

    Initial commit

---
+++ src/main/java/com/greenfox/backend/GreenfoxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.greenfox.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenfoxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenfoxBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenfox/backend/GreenfoxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenfox.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenfoxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
